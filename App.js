65de32a2aa317be62bc7be797e0d45e71ffeaad7
TannerJ0nes <127117143+TannerJ0nes@users.noreply.github.com>
2024-01-18 18:21:51 -0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+//Written By Tanner Jones
+//Date Jan 18th 2024
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Header from './Header';
+import AboutMe from './AboutMe';
+import Expertise from './Expertise';
+import AboutPage from './AboutPage';
+import Works from './Works';
+import ContactMe from './ContactMe'; 
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/contact" element={<ContactMe />} /> {/* Use element prop instead of component */}
+          <Route index element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <AboutMe />
+      <Expertise />
+    </>
+  );
+}
+
+export default App;
